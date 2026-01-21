00d0be1d5c73ebd216e5555959e5ef6252b96bd7
Your Name <you@example.com>
2026-01-21 16:48:56 +0700

    Test p06

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,6 +1,11 @@
+import { create } from "domain";
+
 function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
-  // Write your code below
+  return [...new Set(arr1 && arr2)]
 }
 
 const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
+
+const UniqueNum = getUniqueNumbers(arr1,arr2);
+console.log(UniqueNum);
