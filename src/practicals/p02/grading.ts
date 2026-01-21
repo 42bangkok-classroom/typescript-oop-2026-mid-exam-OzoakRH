9cacdbaa021318d880260fb1b050502066e20fcf
Your Name <you@example.com>
2026-01-21 15:08:04 +0700

    p02

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,26 @@
-// Write your code below
+const reciveinput = process.argv[2];
+const pointScore = Number(reciveinput);
+
+if (Number.isNaN(pointScore) || reciveinput === "") {
+    console.log("Invalid Input")
+}
+else if (pointScore < 0 || pointScore > 100){
+    console.log("Invalid Input")
+}
+else {
+    if (pointScore >= 80){
+        console.log("Grade is A")
+    }
+    else if (pointScore >= 70){
+        console.log("Grade is B")
+    }
+    else if (pointScore >= 60){
+        console.log("Grade is C")
+    }
+    else if (pointScore >= 50){
+        console.log("Grade is D")
+    }
+    else{
+        console.log("Grade is F")
+    }
+}
