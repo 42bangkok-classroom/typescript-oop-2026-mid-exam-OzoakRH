c42f4e49ac397b4aee565a4a890a263aab5912ed
Your Name <you@example.com>
2026-01-21 16:15:43 +0700

    p03 update

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,20 +1,29 @@
-const input = process.argv[2]
-const number = Number(input)
+export{}
 
-if (Number.isNaN(number) || input=== "") {  
-}
-else if (number <= 0){
-}
-else {
-    if (number == number/3 || number == number / 5) {
-        console.log("FizzBuzz")
+const input = process.argv[2]
+const numberInput = Number(input)
+
+for (let i = 1; i <= numberInput; i++) {
+    if(Number.isNaN(numberInput) || input === ""){
     }
-    else if (number == number / 3) {
-        console.log("Fizz")
+    else if (numberInput <= 0 ){
     }
-    else if (number == number / 5) {
-        console.log("Buzz")
-    } else {
-        console.log(number)
+    else{
+        if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+      } else if (i % 3 === 0) {
+        console.log("Fizz");
+      } else if (i % 5 === 0) {
+        console.log("Buzz");
+      } else {
+        console.log(i);
+      }
     }
 }
+
+   
+   
+    
+
+
+  
