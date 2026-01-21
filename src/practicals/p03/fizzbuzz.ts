6eb8867a649e16a2808958b19666bcbd2b2e11a2
Your Name <you@example.com>
2026-01-21 15:57:03 +0700

    Test p0&3

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,20 @@
-// Write your code below
+const input = process.argv[2]
+const number = Number(input)
+
+if (Number.isNaN(number) || input=== "") {  
+}
+else if (number <= 0){
+}
+else {
+    if (number == number/3 || number == number / 5) {
+        console.log("FizzBuzz")
+    }
+    else if (number == number / 3) {
+        console.log("Fizz")
+    }
+    else if (number == number / 5) {
+        console.log("Buzz")
+    } else {
+        console.log(number)
+    }
+}
--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,14 @@
-// Write your code below
+export{}
+
+const Input:number =  Number(process.argv[2]);
+if (Number.isNaN(Input)|| Input <=0){
+  process.exit();
+}
+
+for (let i = 1; i <= Input; i++){
+  let result = '';
+		for(let j = 0; j< i; j++){
+      result += Input 
+    }
+    console.log(result)
+}
